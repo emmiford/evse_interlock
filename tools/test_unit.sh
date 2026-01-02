cb589fdaf49c684ee333ed50ad8316a522f26bc7
Emily Trash <emilystrash@gmail.com>
2026-01-02 00:01:23 -0700

    Fix unit test run target path

--- tools/test_unit.sh
+++ tools/test_unit.sh
@@ -7,4 +7,8 @@ BUILD_DIR="${ROOT_DIR}/build-tests/gpio_event"
 BOARD="${BOARD:-native_posix}"
 
 west build -p always -b "${BOARD}" -d "${BUILD_DIR}" "${TEST_DIR}"
-west build -t run -d "${BUILD_DIR}"
+if [[ -f "${BUILD_DIR}/gpio_event/build.ninja" ]]; then
+  west build -t run -d "${BUILD_DIR}/gpio_event"
+else
+  west build -t run -d "${BUILD_DIR}"
+fi
